11b233fcb9313e0151d1a55bb3707271a8bcfb8b
chris <jimenezchristopher5@gmail.com>
2024-05-02 15:41:31 -0500

    completed fetch api feature by creating separate routes, but having both lists in the same index page

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ mongoose.connection.on("connected", ()=>{
 app.use(methodOverride("_method"));
 app.use(morgan("dev"));
 app.use(express.static("public"));
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({extended:false})); //When I included both forms in the same POST route handler, the request would not resolve and document was not created because extended was set to false - learned that when set to true it's better for parsing nested objects, part of core Node.js library, and slightly faster
 
 //Start Server
 const port = 4005;
@@ -29,6 +29,7 @@ app.listen(port, ()=>{
 });
 
 //Routes
+    //Home Show Page
 app.get("/", (req, res)=>{
     res.render("home.ejs");
 });
@@ -36,66 +37,57 @@ app.get("/", (req, res)=>{
     //Index Show Page
 app.get("/pokemon", async (req, res)=>{
     const pokemonList = await Pokemon.find({}).sort({name: "asc"});
+    const pokemonArray = await ApiPokemon.find();
     res.render("index.ejs", {
         pokemonList,
+        pokemonArray,
     });
 });
 
-    //Pokedex Show Page
-app.get("/pokemon/api", async (req, res)=>{
-    const pokemonArray = await ApiPokemon.find();
-    res.render("apishow.ejs", {
-        pokemonArray
-    });
-})
-
-    //Add From API
-    app.post("/pokemon/api", async (req, res)=>{
-        const pokedexId = req.body.pokemonId;
-        const url = `https://pokeapi.co/api/v2/pokemon/${pokedexId}`;
-        let pokeName;
-        let pokeAbility;
-        let pokeType;
-        let pokeMove;
-    
-        const pokemonData = async (url) => {
-            try {
-                const response = await fetch(url);
-                // console.log(response.ok); //Checking value of the property .ok of response object
-                if(!response.ok){ //If the value of .ok property is false, then there was an error in fetching the data and we can throw an error to the catch block - operation ends there immediatley and goes to the catch block
-                    throw new Error(`HTTP Status: ${response.status}`);
-                };
-                const data = await response.json();
-                pokeName = data.name; //Name of Pokemon
-                pokeAbility = data.abilities[0].ability.name; //First ability name in array of abilities
-                pokeType = data.types[0].type.name; //First type name in array of types
-                pokeMove = data.moves[1].move.name;
-                const pokemon = await ApiPokemon.create({
-                    apiname: pokeName,
-                    apiability: pokeAbility,
-                    apitype: pokeType,
-                    apimove: pokeMove,
-                });
-                
-                console.log(pokemon);
-            } catch(error) {
-                console.log(error);
-            }
-        }
-        pokemonData(url);
-        res.redirect("/pokemon/api");
-    })
-
-
-
-
-
-
-
-    //Create Show Page
+    //Create Pokemon Box Show Page
 app.get("/pokemon/new", (req, res)=>{
     res.render("new.ejs", {});
 });
+
+    //Add Pokedex Show Page
+app.get("/pokemon/new/api", (req, res)=>{
+    res.render("new-api.ejs");
+})
+
+    //Add From API
+app.post("/pokemon/api", async (req, res)=>{
+    const pokedexId = req.body.pokemonId;
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokedexId}`;
+    let pokeName;
+    let pokeAbility;
+    let pokeType;
+    let pokeMove;
+
+    const pokemonData = async (url) => {
+        try {
+            const response = await fetch(url);
+            if(!response.ok){ //If the value of .ok property is false, then there was an error in fetching the data and we can throw an error to the catch block - operation ends there immediatley and goes to the catch block
+                throw new Error(`HTTP Status: ${response.status}`);
+            };
+            const data = await response.json();
+            pokeName = data.name; //Name of Pokemon
+            pokeAbility = data.abilities[0].ability.name; //First ability name in array of abilities
+            pokeType = data.types[0].type.name; //First type name in array of types
+            pokeMove = data.moves[1].move.name; //Second move name in array of moves
+            const pokemon = await ApiPokemon.create({
+                apiname: pokeName,
+                apiability: pokeAbility,
+                apitype: pokeType,
+                apimove: pokeMove,
+            });
+            res.redirect("/pokemon"); //I initially had res.redirect() outside of the async function, but I had to refresh the browser to see the udpated database - Learned that it had to go inside the async function where the document is created and stored in the database, so that the redirection occurs AFTER the document is created and stored (Outside, it redirected before the document was actually created and stored - hence the need to refresh browser)
+            console.log(pokemon);
+        } catch(error) {
+            console.log(error);
+        }
+    }
+    pokemonData(url);
+})
 
     //POST Route
 app.post("/pokemon/new", async (req, res)=>{
@@ -103,9 +95,6 @@ app.post("/pokemon/new", async (req, res)=>{
         req.body.evolution = true;
     }else{
         req.body.evolution = false;
-    }
-    if(req.body.pokeball === ""){
-        req.body.pokeball = "Normal Pokeball";
     }
     const pokemon = await Pokemon.create(req.body);
     res.redirect("/pokemon");
@@ -116,6 +105,13 @@ app.get("/pokemon/:id", async (req, res)=>{
     const pokemon = await Pokemon.findById(req.params.id);
     res.render("show.ejs", {
         pokemon
+    });
+});
+
+app.get("/pokemon/api/:id", async (req, res)=>{
+    const apiPokemon = await ApiPokemon.findById(req.params.id);
+    res.render("apishow.ejs", {
+        apiPokemon,
     });
 });
 
@@ -132,7 +128,7 @@ app.get("/pokemon/:id/edit", async (req, res)=>{
         pokemon
     });
 })
-
+    //PUT Route
 app.put("/pokemon/:id", async (req, res)=>{
     if(req.body.evolution === "on"){
         req.body.evolution = true;
@@ -144,39 +140,3 @@ app.put("/pokemon/:id", async (req, res)=>{
 })
 
 
-//     //Add From API
-// app.post("/pokemon/api/new", async (req, res)=>{
-//     const pokedexId = req.body.pokemonId;
-//     const url = `https://pokeapi.co/api/v2/pokemon/${pokedexId}`;
-//     let pokeName;
-//     let pokeAbility;
-//     let pokeType;
-//     let pokeMove;
-
-//     const pokemonData = async (url) => {
-//         try {
-//             const response = await fetch(url);
-//             // console.log(response.ok); //Checking value of the property .ok of response object
-//             if(!response.ok){ //If the value of .ok property is false, then there was an error in fetching the data and we can throw an error to the catch block - operation ends there immediatley and goes to the catch block
-//                 throw new Error(`HTTP Status: ${response.status}`);
-//             };
-//             const data = await response.json();
-//             pokeName = data.name; //Name of Pokemon
-//             ability = data.abilities[0].ability.name; //First ability name in array of abilities
-//             type = data.types[0].type.name; //First type name in array of types
-//             move = data.moves[1].move.name;
-//             console.log(pokeName, ability, type, move);
-//         } catch(error) {
-//             console.log(error);
-//         }
-//     }
-//     pokemonData(url);
-//      const pokemon = await ApiPokemon.create({
-//         apiname: pokeName,
-//         apiability: pokeAbility,
-//         apitype: pokeType,
-//         apimove: pokeMove,
-//     },{new: true});
-//     console.log(pokemon)
-//     res.redirect("/pokemon/api");
-// })
