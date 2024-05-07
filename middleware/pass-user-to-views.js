883822107bc50445aaa5c2d2481e4599e1335f88
chris <jimenezchristopher5@gmail.com>
2024-05-07 11:13:35 -0500

    turned res.locals function into async attempt to debug aws

--- middleware/pass-user-to-views.js
+++ middleware/pass-user-to-views.js
@@ -1,6 +1,6 @@
-const passUserToView = (req, res, next)=>{
+const passUserToView = async (req, res, next)=>{
     console.log(req.session.user);
-    res.locals.user = req.session.user ? req.session.user : null;
+    res.locals.user = await req.session.user ? req.session.user : null;
     console.log(res.locals.user)//debug attempt for AWS
     next();
 };
