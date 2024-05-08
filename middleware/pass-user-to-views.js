bb535db9261d750c80bca6851e25c51b259c6eef
chris <jimenezchristopher5@gmail.com>
2024-05-08 07:52:28 -0500

    removed console.log debug

--- middleware/pass-user-to-views.js
+++ middleware/pass-user-to-views.js
@@ -1,7 +1,5 @@
 const passUserToView = async (req, res, next)=>{
-    console.log(req.session.user);
     res.locals.user = await req.session.user ? req.session.user : null;
-    console.log(res.locals.user)//debug attempt for AWS
     next();
 };
 
